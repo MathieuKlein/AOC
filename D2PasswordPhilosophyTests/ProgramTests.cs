d6ded43676a75e2b0204420f532c953d3726f138
Mathieu Klein <mathieu.klein@ia.ca>
2020-12-04 16:30:29 -0500

    Solve D2 + Bonus : Naive method

---
+++ D2PasswordPhilosophyTests/ProgramTests.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using System.Linq;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PasswordPhilosophy.Tests
+{
+    [TestClass]
+    public class ProgramTests
+    {
+        [TestMethod]
+        public void CheckMinMaxTest()
+        {
+            var strings = File.ReadLines(Program.InputTxt).ToList();
+
+            Program.CheckMinMax(strings).Should().Be(454);
+        }
+
+        [TestMethod]
+        public void CheckPositionTest()
+        {
+            var strings = File.ReadLines(Program.InputTxt).ToList();
+
+            Program.CheckPosition(strings).Should().Be(649);
+        }
+    }
+}
