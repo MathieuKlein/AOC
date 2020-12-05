ee82192722a078826bf4033f57b448040a767b6e
Mathieu Klein <mathieu.klein@ia.ca>
2020-12-05 13:31:03 -0500

    Solve D5 + Bonus : naive method

---
+++ D5/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace BinaryBoarding
+{
+    public class Program
+    {
+        public const string InputTxt = "input.txt";
+
+        public static void Main(string[] args)
+        {
+            var strings = File.ReadLines(InputTxt);
+            var seatsId = strings.Select(GetSeatId).ToList();
+            var value = seatsId.Max();
+
+            Console.WriteLine(value);
+
+            var missingSeat = GetMissingSeat(seatsId);
+
+            Console.WriteLine(missingSeat);
+            Console.ReadKey();
+        }
+
+        public static double GetMissingSeat(IEnumerable<double> seatsId)
+        {
+            var ordered = seatsId.OrderBy(x => x).ToList();
+
+            return ordered.Where((x, index) => x + 1 != ordered[index + 1]).First() + 1;
+        }
+
+        public static double GetSeatId(string line)
+        {
+            double column = 0;
+            double row = 0;
+            var i = 6;
+            double j = 2;
+            foreach (var c in line)
+            {
+                switch (c)
+                {
+                    case 'B':
+                        row += Math.Pow(2, i);
+                        i--;
+                        break;
+                    case 'F':
+                        i--;
+                        break;
+                    case 'R':
+                        column += Math.Pow(2, j);
+                        j--;
+                        break;
+                    case 'L':
+                        j--;
+                        break;
+                }
+            }
+
+            var id = row * 8 + column;
+            Debug.WriteLine($"{row} {column} {id}");
+            return id;
+        }
+    }
+}
---
+++ D5Tests/ProgramTests.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using System.Linq;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BinaryBoarding.Tests
+{
+    [TestClass]
+    public class ProgramTests
+    {
+        [DataTestMethod]
+        [DataRow("BFFFBBFRRR", 567)]
+        [DataRow("FFFBBBFRRR", 119)]
+        [DataRow("BBFFBBFRLL", 820)]
+        public void GetSeatId_WhenExample(string seat, int id)
+        {
+            Program.GetSeatId(seat).Should().Be(id);
+        }
+
+        [TestMethod]
+        public void MaxSeatsIdTest()
+        {
+            var strings = File.ReadLines(Program.InputTxt);
+
+            var seatsId = strings.Select(Program.GetSeatId).ToList();
+
+            seatsId.Max().Should().Be(874);
+        }
+
+        [TestMethod]
+        public void GetMissingSeatIdTest()
+        {
+            var strings = File.ReadLines(Program.InputTxt);
+
+            var missingSeat = Program.GetMissingSeat(strings.Select(Program.GetSeatId).ToList());
+
+            missingSeat.Should().Be(594);
+        }
+    }
+}
