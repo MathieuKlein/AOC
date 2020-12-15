d4aefc50c5fa369156a6b15d7c9550498ad492b7
Mathieu Klein <mathieu.klein@ia.ca>
2020-12-15 16:20:52 -0500

    Solve D15 + Bonus

---
+++ D15RambunctiousRecitation/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace D15RambunctiousRecitation
+{
+    public class Program
+    {
+        public const string Input = "18,8,0,5,4,1,20";
+
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var numbers = Input.Split(',');
+
+
+            Console.WriteLine(GetValueAtN(numbers, 2020));
+            Console.WriteLine(GetValueAtN(numbers, 30000000));
+            Console.ReadKey();
+        }
+
+        public static int GetValueAtN(string[] input, int n)
+        {
+            var inputNumbers = input.Select(int.Parse).ToList();
+            var valueLastIndexDict = new Dictionary<int, int>();
+            for (var index = 0; index < inputNumbers.Count - 1; index++)
+                valueLastIndexDict[inputNumbers[index]] = index;
+
+            var lastAdded = inputNumbers[^1];
+            for (var i = inputNumbers.Count; i < n; i++)
+                if (!valueLastIndexDict.ContainsKey(lastAdded))
+                {
+                    valueLastIndexDict[lastAdded] = i - 1;
+                    lastAdded = 0;
+                }
+                else
+                {
+                    var calc = i - 1 - valueLastIndexDict[lastAdded];
+                    valueLastIndexDict[lastAdded] = i - 1;
+                    lastAdded = calc;
+                }
+
+            return lastAdded;
+        }
+    }
+}
---
+++ D15RambunctiousRecitationTests/ProgramTests.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace D15RambunctiousRecitation.Tests
+{
+    [TestClass]
+    public class ProgramTests
+    {
+        [TestMethod]
+        public void GetValueAt2009Test()
+        {
+            Program.GetValueAtN(Program.Input.Split(','), 2020).Should().Be(253);
+        }
+
+        [TestMethod]
+        public void GetValueAt30000000Test()
+        {
+            Program.GetValueAtN(Program.Input.Split(','), 30000000).Should().Be(13710);
+        }
+    }
+}
