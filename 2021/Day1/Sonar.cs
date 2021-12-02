58b6f0b1c16439493ec7ef7da8f17c27f4c13d3a
Mathieu Klein <mathieu.klein@ia.ca>
2021-12-01 19:19:29 -0500

    Solve D1

---
+++ 2021/Day1/Sonar.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+internal class Sonar
+{
+    private List<int> _input;
+
+    public Sonar(string input)
+    {
+        _input = File.ReadLines(input).Select(x=> int.Parse(x)).ToList();
+    }
+
+    internal int CountNumberOfIncreases()
+    {
+        var previousInput = int.MaxValue;
+        int count = 0;
+
+        foreach (var input in _input)
+        {
+            if (input > previousInput)
+            {
+                count++;
+            }
+            previousInput = input;
+        }
+        return count;
+    }
+
+    internal int CountNumberOfIncreasesOfSumOf3()
+    {
+        var previousInput = int.MaxValue;
+        int count = 0;
+
+        for (int i = 0; i < _input.Count; i++)
+        {
+            int input = _input.Skip(i).Take(3).Sum();
+            if (input > previousInput)
+            {
+                count++;
+            }
+            previousInput = input;
+        }
+        return count;
+    }
+}
